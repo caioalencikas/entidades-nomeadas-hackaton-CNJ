02a4de4bec4c00c9a7b1f44fca68c91ed5cda643
Caio Alencikas <caio.alencikas@gmail.com>
2020-10-21 18:41:28 -0300

    Feat telas: adionado telas em HTML simulando a aplicação do CNJ com as alterações

---
+++ telas/javascript/funcoes.js
@@ -0,0 +1,5 @@
+function verificarAcao(value) {
+    if (value == 0) {
+        window.alert("Atenção: é provável que este não seja o movimento correto. Recomendamos verificar novamente.")
+    }
+}
